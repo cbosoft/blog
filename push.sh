02d604df8f8858faf7a7bbdfaa500f0ac3f1fe9e
Christopher Boyle <chrismjboyle@gmail.com>
2022-02-20 20:12:16 +0000

    update build and push script

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 ALRIGHT_THEN="$(cat ~/.alright_then)"
+FTP_DEST="$(cat ~/.ftp_dest)"
 TARGET="blog"
 
 jekyll build -d "${TARGET}" || exit 1
 
-find "${TARGET}" -type f -exec curl -u "cmjbteo:${ALRIGHT_THEN}" --ftp-create-dirs -T {} ftp://ftp.cluster031.hosting.ovh.net/www/\{\} \;
+find "${TARGET}" -type f -exec curl -u "cmjbteo:${ALRIGHT_THEN}" --ftp-create-dirs -T {} ftp://"${FTP_DEST}"/\{\} \;
