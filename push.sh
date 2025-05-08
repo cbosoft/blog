6e97c3ee83c81a91c391985acf10324c7c79dafa
Christopher Boyle <christopher.boyle.101@strath.ac.uk>
2025-05-08 22:35:36 +0100

    Add some informative messages to push; use lftp instead of curl for faster uploads

--- push.sh
+++ push.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+error() {
+  echo "$1"
+  exit 1
+}
+
 JEKYLL="bundle exec jekyll"
 
 ALRIGHT_THEN="$(cat ~/.alright_then)"
 FTP_DEST="$(cat ~/.ftp_dest)"
 TARGET="blog"
 
-${JEKYLL} build -d "${TARGET}" || exit 1
+${JEKYLL} build -d "${TARGET}" || error "Build failed."
 
-find "${TARGET}" -type f -exec curl -u "cmjbteo:${ALRIGHT_THEN}" --ftp-create-dirs -T {} ftp://"${FTP_DEST}"/\{\} \;
+lftp -c "open ftp://cmjbteo:${ALRIGHT_THEN}@${FTP_DEST}; cd www/blog; lcd _site; mirror -R --delete" || error "Push failed."
+#find "${TARGET}" -type f -exec curl -u "cmjbteo:${ALRIGHT_THEN}" --ftp-create-dirs -T {} ftp://"${FTP_DEST}"/\{\} \;
+
+echo "Done!"
