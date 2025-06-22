5538a3c605f9e56dd2ae99113732f2c5e5156516
Christopher Boyle <christopher.boyle.101@strath.ac.uk>
2025-06-22 14:13:02 +0100

    make sure to push the correct folder ffs

--- push.sh
+++ push.sh
@@ -11,9 +11,10 @@ ALRIGHT_THEN="$(cat ~/.alright_then)"
 FTP_DEST="$(cat ~/.ftp_dest)"
 TARGET="blog"
 
+${JEKYLL} clean -d "${TARGET}" || error "Clean failed."
 ${JEKYLL} build -d "${TARGET}" || error "Build failed."
 
-lftp -c "open ftp://cmjbteo:${ALRIGHT_THEN}@${FTP_DEST}; cd www/blog; lcd _site; mirror -R --delete" || error "Push failed."
+lftp -c "open ftp://cmjbteo:${ALRIGHT_THEN}@${FTP_DEST}; cd www/blog; lcd ${TARGET}; mirror -R --delete" || error "Push failed."
 #find "${TARGET}" -type f -exec curl -u "cmjbteo:${ALRIGHT_THEN}" --ftp-create-dirs -T {} ftp://"${FTP_DEST}"/\{\} \;
 
 echo "Done!"
